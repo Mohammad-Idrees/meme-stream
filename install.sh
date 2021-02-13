9e30fa61ff9e636ff1eca8c5b62303cc3c9022de
idrees <idreesmohammad976@gmail.com>
2021-02-13 19:27:01 +0530

    install.sh changes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt install -y python3-pip
 sudo apt-get install -y python3.8
-python3.8 -m pip install --user --upgrade pip
+python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
 sudo apt-get install -y postgresql postgresql-contrib
