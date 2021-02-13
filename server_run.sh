fd493a4986455fd2b9f6fc9715cfb68f40100644
idrees <idreesmohammad976@gmail.com>
2021-02-13 19:09:06 +0530

    hypercorn added

--- server_run.sh
+++ server_run.sh
@@ -1,4 +1,4 @@
 sudo -u postgres createdb data
 sudo -u postgres createuser us
 sudo -u postgres psql --command '\password'
-uvicorn main:app --reload --port 8081
+hypercorn main:app --reload --port 8081
