eb5cb4e798f710800fd8066c2857f320370f88cf
idrees <idreesmohammad976@gmail.com>
2021-02-13 16:40:29 +0530

    install.sh changes

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-sudo apt-get install -y python3-pip
-sudo apt-get install -y postgresql postgresql-contrib
+sudo apt update
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+sudo update-alternatives --config python3
+python3 -m pip install --user --upgrade pip
 pip3 install -r requirements.txt
+sudo apt-get install -y postgresql postgresql-contrib
