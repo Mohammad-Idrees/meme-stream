de7e39f63226aa7ad638dca1675c80df25ac6ab3
idrees <idreesmohammad976@gmail.com>
2021-02-14 12:48:28 +0530

    uvicorn command changed

--- server_run.sh
+++ server_run.sh
@@ -3,4 +3,4 @@ sudo -u postgres createuser xmemeadmin
 sudo -u postgres createdb xmeme
 sudo -u postgres psql -c "alter user xmemeadmin with encrypted password 'pass';"
 sudo -u postgres psql -c "grant all privileges on database xmeme to xmemeadmin;"
-uvicorn main:app --reload --port8081 
+uvicorn main:app --reload --port 8081 
